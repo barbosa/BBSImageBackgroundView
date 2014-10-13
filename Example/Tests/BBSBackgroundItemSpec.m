3ba7ef48f860d5dd40328421df5d0aa26c9e9b18
Gustavo Barbosa <gustavocsb@gmail.com>
2014-10-12 21:19:15 -0300

    Fix to run tests.

---
+++ Example/Tests/BBSBackgroundItemSpec.m
@@ -0,0 +1,38 @@
+//
+//  BBSBackgroundItemSpec.m
+//  BBSImageBackgroundView
+//
+//  Created by Gustavo Barbosa on 10/12/14.
+//  Copyright 2014 Gustavo Barbosa. All rights reserved.
+//
+
+#import "Specta.h"
+#import "BBSBackgroundItem.h"
+
+
+SpecBegin(BBSBackgroundItem)
+
+describe(@"BBSBackgroundItem", ^{
+    
+    beforeAll(^{
+
+    });
+    
+    beforeEach(^{
+ 
+    });
+    
+    it(@"", ^{
+        assert(NO);
+    });  
+    
+    afterEach(^{
+
+    });
+    
+    afterAll(^{
+
+    });
+});
+
+SpecEnd
